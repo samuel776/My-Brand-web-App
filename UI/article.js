ebecd8d94788b8d66a909261319da97ec159fd5d
Samuel776 <hbnzsml@gmail.com>
2020-10-23 07:04:49 +0200

    ft: create article
    this will help the admin to create article and the user have access on  it
    
    [delivers] TFQPuPtq

--- UI/article.js
+++ UI/article.js
@@ -8,4 +8,28 @@ fetch(`${url}/posts`, {
     "auth-token": savedToken,
   },
   
-}).then(res =>res.json()).then(data =>console.log(data)) 
+}).then(res =>res.json()).then(data =>console.log(data)).catch((error) => console.log(error));
+
+function createArticle(data){
+  fetch(`${url}/posts`, {
+    method:'POST',
+    headers:{
+      "content-type":"application/json",
+    "auth-token": savedToken,
+    },
+    body:JSON.stringify(data)
+  }).then(res => res.json()).then(data => console.log(data)).catch((error) => console.log(error));
+}
+
+const submitForm = document.getElementById("myform")
+submitForm.onsubmit = (e) =>{
+e.preventDefault();
+  const title = myform["myTitle"].value;
+  const description = myform["message"].value;
+
+  const article = {
+    title,
+    description,
+  };
+  createArticle(article);
+}
